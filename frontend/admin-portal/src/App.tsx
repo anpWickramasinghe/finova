10d947d37f6a871287c097e847c19e627f605d69
nethmina1028 <nethmina1028@gmail.com>
2025-12-17 20:11:53 +0530

    Feat:Portal updated

---
+++ frontend/admin-portal/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Admin Portal</h1>
+      <p>Welcome to the Employee Portal.</p>
+    </div>
+  )
+}
+
+export default App
